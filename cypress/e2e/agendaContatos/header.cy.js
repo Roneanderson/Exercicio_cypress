75ddb8946ef17bca511a87fb2c2f6482f7e06389
Rone <roneanderson469@gmail.com>
2025-01-01 14:47:56 -0300

    projeto cypress alterando contato, adicionando contato, removendo contato

--- cypress/e2e/agendaContatos/header.cy.js
+++ cypress/e2e/agendaContatos/header.cy.js
@@ -5,30 +5,27 @@ describe("Teste para Agenda de contato", () => {
     cy.visit("https://agenda-contatos-react.vercel.app/");
   });
 
-  it("inserção de contato", () => {
+  it("adicionando contato", () => {
     cy.get('[type="text"]').type("gian", 3);
     cy.get('[type="email"]').type("gian@gmail.com.br", 3);
     cy.get('[type="tel"]').type("3981152145", 3);
-    cy.get(".adicionar").click = { multiple: true };
-    cy.get(".edit").click();
-    cy.get(".alterar").click();
-    cy.get(".cancelar").click();
+    cy.get(".adicionar").click();
     cy.get("input").should("have.length", 3);
   });
 
-  it("deletando contato de contato", () => {
-    cy.get('[type="text"]').type("gian", 9);
-    cy.get('[type="email"]').type("gian@gmail.com.br", 9);
-    cy.get('[type="tel"]').type("3981152145", 3);
-    cy.get(".adicionar").click();
-    cy.get(":nth-child(2) > .sc-gueYoa > .delete").click();
-  });
-
-  it("removendo de contato", () => {
+  it("alteração de contato", () => {
     cy.get('[type="text"]').type("gian", 3);
     cy.get('[type="email"]').type("gian@gmail.com.br", 3);
     cy.get('[type="tel"]').type("3981152145", 3);
     cy.get(":nth-child(2) > .sc-gueYoa > .edit").click();
-    cy.get(".cancelar").click();
+    cy.get(".alterar").click();
+  });
+
+  it("removendo contato", () => {
+    cy.get('[type="text"]').type("gian", 3);
+    cy.get('[type="email"]').type("gian@gmail.com.br", 3);
+    cy.get('[type="tel"]').type("3981152145", 3);
+    cy.get(".adicionar").click();
+    cy.get(":nth-child(2) > .sc-gueYoa > .delete").click();
   });
 });
